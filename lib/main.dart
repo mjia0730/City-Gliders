02a835bdd3eb6aa71f0cde78d0b0c298e2da3029
zhiqing0923 <chanzhiqing923@gmail.com>
2024-06-02 01:29:14 +0800

    changes to font size

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChatScreen(),
+      home: const ChatScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: GoogleFonts.notoSans().fontFamily),
     );
